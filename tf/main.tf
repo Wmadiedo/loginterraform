895fd5b3eb5e9fb69353ed502e8d981c627e9f0f
BreynerPearson <126908117+BreynerPearson@users.noreply.github.com>
2025-05-27 15:05:51 -0500

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -87,7 +87,3 @@ resource "azurerm_static_site" "frontend" {
 output "public_ip" {
   value = azurerm_public_ip.pip.ip_address
 }
-
-output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
-}
