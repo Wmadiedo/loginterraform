f7f5b406b8646652e02c38e22df9b7af04b861df
BreynerPearson <126908117+BreynerPearson@users.noreply.github.com>
2025-05-27 08:52:14 -0500

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,22 +1,20 @@
 provider "azurerm" {
   features {}
 }
-resource "azurerm_static_site" "frontend" {
-  name                = "loginejemplo"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = "East US 2"
-  sku_name            = "Free"
-
-  app_location         = "." 
-  output_location      = "."      # no build, plain HTML
-  index_document       = "index.html"
-  error_404_document   = "index.html"
-}
 
 resource "azurerm_resource_group" "main" {
   name     = "webapp-rg"
   location = "East US 2"
 }
+
+
+resource "azurerm_public_ip" "pip" {
+  name                = "prueba-pip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+}
+
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "prueba-vnet"
@@ -32,6 +30,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+
 resource "azurerm_network_interface" "nic" {
   name                = "prueba-nic"
   location            = azurerm_resource_group.main.location
@@ -39,30 +38,24 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                    = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
-resource "azurerm_public_ip" "pip" {
-  name                = "prueba-pip"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
-}
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "prueba-vm"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  size                = "Standard_B1s"
-  admin_username      = "wollmanhoyos"
+  name                  = "prueba-vm"
+  resource_group_name   = azurerm_resource_group.main.name
+  location              = azurerm_resource_group.main.location
+  size                  = "Standard_B1s"
+  admin_username        = "wollmanhoyos"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   admin_ssh_key {
     username   = "wollmanhoyos"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("${path.module}/id_rsa.pub") # Asegúrate de tener este archivo aquí
   }
 
   os_disk {
@@ -76,6 +69,23 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "18.04-LTS"
     version   = "latest"
   }
+}
+
+resource "azurerm_static_site" "frontend" {
+  name                = "loginejemplo"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku_name            = "Free"
+
+  app_location       = "../webapp"
+  output_location    = "../webapp"
+  index_document     = "index.html"
+  error_404_document = "index.html"
+}
+
+
+output "public_ip" {
+  value = azurerm_public_ip.pip.ip_address
 }
 
 output "public_ip" {
