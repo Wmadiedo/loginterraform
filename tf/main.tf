936c4b33b495a2908de8cef9b72cb4191deb6926
luisfer1397 <45372504+luisfer1397@users.noreply.github.com>
2025-05-27 16:44:23 -0500

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -7,14 +7,12 @@ resource "azurerm_resource_group" "main" {
   location = "East US 2"
 }
 
-
 resource "azurerm_public_ip" "pip" {
   name                = "prueba-pip"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Dynamic"
 }
-
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "prueba-vnet"
@@ -30,7 +28,6 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-
 resource "azurerm_network_interface" "nic" {
   name                = "prueba-nic"
   location            = azurerm_resource_group.main.location
@@ -44,7 +41,6 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-
 resource "azurerm_linux_virtual_machine" "vm" {
   name                  = "prueba-vm"
   resource_group_name   = azurerm_resource_group.main.name
@@ -55,7 +51,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "wollmanhoyos"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD..." # <-- reemplaza con tu clave
   }
 
   os_disk {
@@ -70,19 +66,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
-
-resource "azurerm_static_site" "frontend" {
-  name                = "loginejemplo"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  sku_name            = "Free"
-
-  app_location       = "../webapp"
-  output_location    = "../webapp"
-  index_document     = "index.html"
-  error_404_document = "index.html"
-}
-
 
 output "public_ip" {
   value = azurerm_public_ip.pip.ip_address
